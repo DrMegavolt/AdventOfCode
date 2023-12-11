144314320a88bb0ffd8bb68793e9917d2b411ac5
Serhii Kuts <skuts@servicetitan.com>
2023-12-10 17:57:12 -0800

    day 10; part2 fixed

--- 2023/day10/main.py
+++ 2023/day10/main.py
@@ -2,8 +2,8 @@ import os
 import collections
 from time import sleep
 folder = os.path.dirname(__file__)
-m = open(folder +  "/input7.txt", "r").read().splitlines()
-
+m = open(folder +  "/input1.txt", "r").read().splitlines()
+original_m = m.copy()
 width = len(m[0])
 height = len(m)
 # find S 
@@ -13,23 +13,6 @@ def print_map(m):
     for y in range(len(m)):
         for x in range(len(m[y])):
             print(m[y][x], end='')
-        print()
-
-def print_map_zoom(m, pos_x, pos_y, zoom, just=5):
-    y_min = max(0, pos_y - zoom)
-    y_max = min(height, pos_y + zoom)
-    x_min = max(0, pos_x - zoom)
-    x_max = min(width, pos_x + zoom)
-    for y in range(y_min, y_max):
-        for x in range(x_min, x_max):
-            s = str(m[y][x])
-            if x == pos_x and y == pos_y:
-                s = '[' + s + ']'
-            print(s.ljust(just), end='')
-            # if m[y][x] > 0:
-            #     print('*', end='')
-            # else:
-            #     print('.', end='')
         print()
 
 distances = []
@@ -99,16 +82,14 @@ for y in range(height):
         if distances[y][x] > max_distance:
             max_distance = distances[y][x]
             max_distance_pos = [x,y]
-print("PART1: end pos = ", max_distance_pos)
 print("PART1:", max_distance)
+print('----')
 
 def walk_loop(x1, y1, path):
     pos_x = x1
     pos_y = y1
     while m[pos_y][pos_x] != 'S':
-        # sleep(0.1)
         prev = path[-1]
-        # print("walk_loop:", pos_x, pos_y, prev, m[pos_y][pos_x])
         path.append([pos_x, pos_y])
         if m[pos_y][pos_x] == '-':
             if prev[0] == pos_x-1: # came from left
@@ -147,62 +128,47 @@ initial_directions = collections.deque()
 init_deque(start, initial_directions)
 
 [x_1,y_1, d] = initial_directions[0] # it is a loop so no matter which of 2 directions we start
-print("initial_directions:", initial_directions)
-p = walk_loop(x_1,y_1, [start])
-for i in range(len(p)):
-    [x,y] = p[i]
+loop_path = walk_loop(x_1,y_1, [start])
+
+# print_map(m)
+
+# the previous loop is not enough, we need to test all the points inside the map
+# since the path is a loop all the dots inside will have at least 1 border from each direction 
+# F-7    f-7f7 
+# |.| or |.||| 1+1 or 1+3
+# L-J    l-jlj 
+# same for top and bottom
+
+for i in range(len(loop_path)):
+    [x,y] = loop_path[i]
     m[y] = m[y][:x] + '&' + m[y][x+1:]
+potential_hideouts = []
+for y in range(height):
+    for x in range(width):
+        if m[y][x] != '&':
+            potential_hideouts.append([x,y])
 
+hideouts_counter = 0
+for spot in potential_hideouts:
+    # [x,y] = spot
 
-print_map(m)
+    line = list(filter(lambda coord: coord[1]==spot[1], loop_path))
+    # sort by x
+    line.sort(key=lambda coord: coord[0])
 
-for y in range(10):
-    potential_hideouts = []
-    visited = []
-    for y in range(height):
-        visited.append([0] * width)
-        for x in range(width):
-            if m[y][x] != '&':
-                potential_hideouts.append([x,y])
+    # count borders before and after coord(.) B.AAA |.||| 1+3
+    before = list(filter(lambda coord: coord[0] < spot[0], line))
+    bf_pipes = list(map(lambda bf: original_m[bf[1]][bf[0]], before))
+    bf_pipes = ''.join(list(filter(lambda t: t != '-', bf_pipes)))
+    after = list(filter(lambda coord: coord[0] > spot[0], line))
+    af_pipes = list(map(lambda af: original_m[af[1]][af[0]], after))
+    af_pipes = ''.join(list(filter(lambda t: t != '-', af_pipes)))
 
-    for ph in potential_hideouts:
-        
-        if visited[ph[1]][ph[0]] == 1:
-            continue
-        q = collections.deque()
-        q.append(ph)
+    # L7 and FJ are the same vertical pipes so we can replace them with |
+    bf_pipes = bf_pipes.replace('L7', '|').replace('FJ', "|").replace('SJ', "|")
+    af_pipes = af_pipes.replace('L7', '|').replace('FJ', "|").replace('SJ', "|")
+    if len(bf_pipes) % 2 == 1 and len(af_pipes) % 2 == 1:
+        hideouts_counter += 1
 
-        while len(q) > 0:
-            [x,y] = q.popleft()
-            
-            if visited[y][x] == 1:
-                continue
-
-            visited[y][x] = 1
-            for i in range(-1,2):
-                for j in range(-1,2):
-                    if x+i < 0 or x+i >= width or y+j < 0 or y+j >= height:
-                        m[y] = m[y][:x] + '0' + m[y][x+1:]
-                        continue
-                    if m[y+j][x+i] == '&':
-                        visited[y+j][x+i] = 1
-                    if m[y+j][x+i] == '0':
-                        visited[y+j][x+i] = 1
-                        m[y] = m[y][:x] + '0' + m[y][x+1:]
-                    
-                    q.append([x+i,y+j])
-
-
-
-
-        print("potential_hideouts:", ph)
-
-    inside = 0
-    for y in range(height):
-        for x in range(width):
-            if m[y][x] == '0' or m[y][x] == '&':
-                continue
-            inside += 1
-
-print_map(m)
-print("PART2:", inside) # 855 too high
+# print_map(original_m)
+print("PART2:", hideouts_counter) # 595 correct
