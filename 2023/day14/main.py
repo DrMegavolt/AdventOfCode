90c4470db8380fdbb83cfae45c9588ad290bb753
Serhii Kuts <skuts@servicetitan.com>
2023-12-13 23:05:14 -0800

    day 14; part2

--- 2023/day14/main.py
+++ 2023/day14/main.py
@@ -2,7 +2,7 @@ import os
 import collections
 import re
 folder = os.path.dirname(__file__)
-data = open(folder + "/input2.txt", "r").read().splitlines()
+data = open(folder + "/input1.txt", "r").read().splitlines()
 
 field = []
 for row in data:
@@ -21,35 +21,113 @@ def print_map(m):
 
 print_map(field)
 
-cycles = 10**0
 
 
-def move_all_north(field):
+def tilt_north(field):
     for x in range(w):
-        bottom = 0
+        north = 0
         for y in range(0, h):
             # print("x:", x, "y:", y, "bottom:", bottom)
             # print_map(field)
             if field[y][x] == '#':
-                bottom = y+1
-                continue;
+                north = y+1
+                continue
             if field[y][x] == 'O':
                 field[y][x] = '.'
-                field[bottom][x] = 'O'
-                bottom +=1
-                continue;
+                field[north][x] = 'O'
+                north += 1
+                continue
 
 
-for i in range(cycles):
-    move_all_north(field)
-print()
-print_map(field)
+def tilt_south(field):
+    for x in range(w):
+        south = h-1
+        for y in range(h-1, -1, -1):
+            # print("x:", x, "y:", y, "bottom:", bottom)
+            # print_map(field)
+            if field[y][x] == '#':
+                south = y-1
+                continue
+            if field[y][x] == 'O':
+                field[y][x] = '.'
+                field[south][x] = 'O'
+                south -= 1
+                continue
 
+def tilt_east(field):
+    for y in range(h):
+        east = w-1
+        for x in range(w-1, -1, -1):
+            # print("x:", x, "y:", y, "bottom:", bottom)
+            # print_map(field)
+            if field[y][x] == '#':
+                east = x-1
+                continue
+            if field[y][x] == 'O':
+                field[y][x] = '.'
+                field[y][east] = 'O'
+                east -= 1
+                continue
+
+def tilt_west(field):
+    for y in range(h):
+        west = 0
+        for x in range(0, w):
+            # print("x:", x, "y:", y, "bottom:", bottom)
+            # print_map(field)
+            if field[y][x] == '#':
+                west = x+1
+                continue
+            if field[y][x] == 'O':
+                field[y][x] = '.'
+                field[y][west] = 'O'
+                west += 1
+                continue
+
+def find_weight(field):
+    weight = 0
+    for y in range(h):
+        for x in range(w):
+            if field[y][x] == 'O':
+                weight += 1 * (h-y)
+    return weight
+cycles = 10**9
+loops = []
+
+loop_candidates = dict()
+loop_size = 0
+i=0
+while i < cycles:
+    i+=1
+    print("cycle:", i)
+    tilt_north(field)
+    if (i==1):
+        print("part1:", find_weight(field))
+
+    # print()
+    # print_map(field)
+    tilt_west(field)
+    # print()
+    # print_map(field)
+    tilt_south(field)
+    # print()
+    # print_map(field)
+    tilt_east(field)
+    weight = find_weight(field)
+    key = str(field)
+    if key in loop_candidates:
+        print("loop found:", i, loop_candidates[key])
+        loop_size = i - loop_candidates[key]
+        print("loop size:", loop_size, "skip:", (cycles - i) // loop_size)
+        i += loop_size * ((cycles - i) // loop_size)
+        print("new cycle:", i)
+        # loops.append(i)
+        # break
+    else:
+        loop_candidates[key] = i
+
+print("loops size:", loop_size)
 # count weight
-part1 = 0
-for y in range(len(field)):
-    for x in range(len(field[y])):
-        if field[y][x] == 'O':
-            part1 += 1 * (h-y)
+part2 = find_weight(field)
 
-print("part1:", part1)
+print("part2:", part2)
